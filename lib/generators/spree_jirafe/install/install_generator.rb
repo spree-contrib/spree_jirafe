bc542925ccae28fe3c66a8279be49dc129aadb40
John Dyer <lbrapid@gmail.com>
2013-12-04 14:25:30 -0500

    Add install generator

---
+++ lib/generators/spree_jirafe/install/install_generator.rb
@@ -0,0 +1,19 @@
+module SpreeJirafe
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      def add_migrations
+        run 'bundle exec rake railties:install:migrations FROM=spree_jirafe'
+      end
+
+      def run_migrations
+        run_migrations = options[:auto_run_migrations] ||
+          ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+        if run_migrations
+          run 'bundle exec rake db:migrate'
+        else
+          puts 'Skipping rake db:migrate, don\'t forget to run it!'
+        end
+      end
+    end
+  end
+end
