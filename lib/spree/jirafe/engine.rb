bd60c7239e29b9ffb34c3aa3e2001795c34251c0
Washington Luiz <huoxito@gmail.com>
2013-12-04 17:04:03 -0300

    Add proper lib file
    And remove lib from autoload paths

--- lib/spree/jirafe/engine.rb
+++ lib/spree/jirafe/engine.rb
@@ -3,8 +3,6 @@ module Spree
     class Engine < ::Rails::Engine
       engine_name "spree_jirafe"
 
-      config.autoload_paths += %W(#{config.root}/lib)
-
       initializer "spree_jirafe.environment", :before => :load_config_initializers do |app|
         Jirafe::Config = Spree::Jirafe::Configuration.new
       end
