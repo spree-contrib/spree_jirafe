407291c4e438b70b21d4ac8fedb85c48444a5b49
Peter Berkenbosch <peter@pero-ict.nl>
2014-10-23 17:56:11 +0200

    we need the dates for the products as well.

--- lib/serializers/spree/wombat/jirafe/product_without_variants_serializer.rb
+++ lib/serializers/spree/wombat/jirafe/product_without_variants_serializer.rb
@@ -6,7 +6,7 @@ module Spree
 
       class ProductWithoutVariantsSerializer < Wombat::ProductSerializer
 
-        attributes :meta_data
+        attributes :meta_data, :created_at, :updated_at
 
         def meta_data
           {
@@ -16,6 +16,16 @@ module Spree
             )
           }
         end
+
+        def created_at
+          object.created_at.getutc.try(:iso8601)
+        end
+
+        def updated_at
+          object.updated_at.getutc.try(:iso8601)
+        end
+
+
       end
 
     end
