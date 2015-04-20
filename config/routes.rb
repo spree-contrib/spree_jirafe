6c785b13a23249cc102aa97634e8139a1792743c
Ricardo Andrés Bello <r@acid.cl>
2015-04-20 18:02:25 -0300

    Remove commented routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do
-    # get 'jirafe_settings', to: "jirafe_settings#edit", as: :jirafe_settings
-    # post 'jirafe_settings', to: "jirafe_settings#update"
     resource :jirafe_settings, only: [:edit, :update]
   end
 end
